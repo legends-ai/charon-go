f7cbd6101b4aade29bf72703b1c13458d1ff9249
Ian Macalinao <me@ian.pw>
2016-11-26 23:45:57 -0600

    Include region in call to match list API

--- riot/client/match_list.go
+++ riot/client/match_list.go
@@ -30,7 +30,7 @@ func (r *API) MatchList(summoner uint64, queues []string, seasons []string) (*Ma
 	}
 
 	resp, err := r.fetchWithParams("matchlist",
-		fmt.Sprintf("%s/v2.2/matchlist/by-summoner/%d", r.apiLol, summoner), vals, true)
+		fmt.Sprintf("%s/%s/v2.2/matchlist/by-summoner/%d", r.apiLol, r.Region, summoner), vals, true)
 	if err != nil {
 		return nil, err
 	}
